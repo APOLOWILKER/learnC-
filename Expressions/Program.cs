fa4ba1a203b6a40e5a420c3c0389959150267ba8
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-02-25 13:29:02 -0300

    expression data in C#

---
+++ Expressions/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("a" == "a"); // true
+Console.WriteLine("a" == "A"); // false
+Console.WriteLine(1 == 2); // false
+string myValue = "a";
+Console.WriteLine(myValue == "a"); // true
