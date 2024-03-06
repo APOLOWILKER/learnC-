1edc7f5c75e3bab566fcaa08b1e9921040c15aa7
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-05 23:45:19 -0300

    finish case boolean

---
+++ Conditional/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+  * O operador condicional é uma forma compacta de escrever uma instrução if-else.
+  * Ele é útil quando você deseja atribuir um valor a uma variável com base em uma condição.
+  * 
+  * O operador condicional é representado por um ponto de interrogação (?) e dois pontos (:).
+  * 
+  * A sintaxe do operador condicional é a seguinte:
+  * 
+  *      condition ? first_expression : second_expression;
+  * 
+  * Se a condição for verdadeira, a primeira expressão será avaliada e retornada; caso contrário, a segunda expressão será avaliada e retornada.
+  * 
+  * O operador condicional é frequentemente usado para atribuir um valor a uma variável com base em uma condição.
+  * 
+  *      int discount = saleAmount > 1000 ? 100 : 50;
+  * 
+  * O código acima atribui 100 à variável discount se saleAmount for maior que 1000; caso contrário, atribui 50.
+  * 
+  * O operador condicional também pode ser usado em uma expressão de saída para imprimir o valor de uma variável com base em uma condição.
+  * 
+  *      Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+  * 
+  * O código acima imprime "Discount: 100" se saleAmount for maior que 1000; caso contrário, imprime "Discount: 50".
+*/
+
+int randonNumber = new Random().Next(0,2);
+Console.WriteLine($"Número: {randonNumber}");
+Console.WriteLine($"Moeda: {(randonNumber == 0 ? "Cara" : "Coroa")}");
---
+++ Conditional/README.md
@@ -0,0 +1,26 @@
+Desafio de código: escrever um código para exibir o resultado de um lançamento de moeda
+Veja os seguintes requisitos do desafio:
+
+Verifique se você tem o arquivo Program.cs vazio aberto no Visual Studio Code.
+
+Se necessário, abra o Visual Studio Code e conclua as seguintes etapas para preparar o arquivo Program.cs no Editor:
+
+No menu Arquivo, selecione Abrir Pasta.
+
+Use a caixa de diálogo Abrir Pasta para navegar e, em seguida, abra a pasta CsharpProjects.
+
+No painel EXPLORER do Visual Studio Code, selecione Program.cs.
+
+No menu Seleção do Visual Studio Code, escolha Selecionar Tudo e pressione a tecla Delete.
+
+Use a classe Random para gerar um valor.
+
+Considerar o intervalo de números necessários.
+
+Com base no valor gerado, usar o operador condicional para exibir heads ou tails.
+
+As chances de que o resultado seja cara ou coroa devem ser de 50% cada.
+
+Seu código deve ser fácil de ler, mas ter o menor número possível de linhas.
+
+Você deve ser capaz de obter o resultado desejado em três linhas de código.
