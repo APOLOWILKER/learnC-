dd51a34d6127ec539bef122c59bd3035c466f2dd
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-12 22:02:05 -0300

    block code without curly brackets

--- ScopeVariable/ScopeVariable/Program.cs
+++ ScopeVariable/ScopeVariable/Program.cs
@@ -1,12 +1,3 @@
 ﻿bool flag = true;
-int value = 5;
-
-if (value > 0)
-{
-    int value2 = 6;
-    value += value2;
-    Console.WriteLine($"Inside the code block: {value2}");
-}
-
-// value = 10;
-Console.WriteLine($"Outside the code block: {value}");
+if (flag)
+    Console.WriteLine(flag);
--- ScopeVariable/ScopeVariable/obj/Debug/net8.0/ScopeVariable.AssemblyInfo.cs
+++ ScopeVariable/ScopeVariable/obj/Debug/net8.0/ScopeVariable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScopeVariable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edc7f5c75e3bab566fcaa08b1e9921040c15aa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63bb3135b9c539ac4fb30199523a6ac3929fffda")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScopeVariable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScopeVariable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
